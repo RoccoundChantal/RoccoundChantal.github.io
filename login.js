13b5fbc2068bfae028676e13132bcc0c3da5147b
Rocco & Chantal <info.lowalt@gmail.com>
2026-09-15 10:51:33 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,18 @@
+function login() {
+    const pw = prompt("Bitte Passwort eingeben:");
+    if (pw === "Musik2026") {
+        localStorage.setItem("loggedIn", "true");
+        location.reload();
+    } else {
+        alert("Falsches Passwort");
+    }
+}
+
+function checkLogin() {
+    if (localStorage.getItem("loggedIn") === "true") {
+        document.querySelectorAll(".protected").forEach(el => el.classList.remove("hidden"));
+    }
+}
+
+checkLogin();
 
